3bcec5b0652e532f0c8974b8a32f77e7c2cc02b6
tzw7701 <tzw7701@gmail.com>
2020-12-24 14:26:04 +0200

    added arrival page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React from 'react';
 import AddAppointment from './components/AddAppointment';
+import IsSoldierArrived from './components/IsSoldierArrived';
 import Container from '@material-ui/core/Container';
 import {Route, Switch} from 'react-router-dom';
 import AppBar from '@material-ui/core/AppBar';
@@ -8,6 +9,7 @@ import Palmahim from './Bacha_30_Palmachim.png';
 import Typography from '@material-ui/core/Typography';
 import Avatar from '@material-ui/core/Avatar';
 
+const Component1 = () => <div>1</div>;
 const Component2 = () => <div>2</div>;
 const Component3 = () => <div>3</div>;
 const Component4 = () => <div>4</div>;
@@ -26,6 +28,7 @@ function App() {
             </AppBar>
 
             <Route exact path="/" component={AddAppointment} />
+            <Route path="/1" component={IsSoldierArrived} />
             <Route path="/2" component={Component2} />
             <Route path="/3" component={Component3} />
             <Route path="/4" component={Component4} />
