d6b18e3738a23a584ea406906af25afc6e139d5b
AmitFeldman <amit123feldman@gmail.com>
2020-12-24 14:52:07 +0200

    started schedule

--- src/App.js
+++ src/App.js
@@ -7,29 +7,27 @@ import AppBar from '@material-ui/core/AppBar';
 import Palmahim from './Bacha_30_Palmachim.png';
 import Typography from '@material-ui/core/Typography';
 import Avatar from '@material-ui/core/Avatar';
+import ScheduleView from './components/ScheduleView';
 
-const Component2 = () => <div>2</div>;
 const Component3 = () => <div>3</div>;
 const Component4 = () => <div>4</div>;
 
 function App() {
   return (
     <div className="App">
+      <AppBar position="sticky">
+        <Avatar alt="Palmahim" src={Palmahim} />
+        <Typography variant="h6">
+          ברוכים הבאים למרכז החיסונים בפלמחים
+        </Typography>
+      </AppBar>
+
       <Container>
         <Switch>
-          <div>
-            <AppBar>
-              <Avatar alt="Palmahim" src={Palmahim} />
-              <Typography variant="h6">
-                ברוכים הבאים למרכז החיסונים בפלמחים
-              </Typography>
-            </AppBar>
-
-            <Route exact path="/" component={AddAppointment} />
-            <Route path="/2" component={Component2} />
-            <Route path="/3" component={Component3} />
-            <Route path="/4" component={Component4} />
-          </div>
+          <Route exact path="/" component={AddAppointment} />
+          <Route path="/schedule" component={ScheduleView} />
+          <Route path="/3" component={Component3} />
+          <Route path="/4" component={Component4} />
         </Switch>
       </Container>
     </div>
---
+++ src/components/ScheduleView.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import {
+  Grid,
+  Typography,
+  Divider,
+  TextField,
+  ListItem,
+  List,
+} from '@material-ui/core';
+
+const users = [{id: 1}, {id: 2}, {id: 3}, {id: 4}];
+const stations = [
+  {id: 1, name: 'A', currentUserId: 6},
+  {id: 2, name: 'B', currentUserId: 8},
+  {id: 3, name: 'C'},
+];
+
+const Title = ({text}) => {
+  return (
+    <>
+      <Typography variant="h4">{text}</Typography>
+      <Divider />
+    </>
+  );
+};
+
+const ScheduleView = () => {
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={4}>
+        <Title text={'תחנות'} />
+
+        <List>
+          {stations.map(({id, name, currentUserId}) => (
+            <ListItem key={id}>
+              <Typography>
+                {name} - {currentUserId ? currentUserId : 'פנויה'}
+              </Typography>
+            </ListItem>
+          ))}
+        </List>
+      </Grid>
+
+      <Grid item xs={8}>
+        <Title text={'ממתינים בתור'} />
+
+        <List>
+          {users.map(({id}) => (
+            <ListItem key={id}>
+              <Typography>{id}</Typography>
+            </ListItem>
+          ))}
+        </List>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default ScheduleView;
