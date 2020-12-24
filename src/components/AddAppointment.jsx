e722c13966e208e6d59380e2c8322d9e0be91699
AlonShirion <shirion.alon@gmail.com>
2020-12-24 10:29:06 +0200

    First try add appointment form

--- src/components/AddAppointment.jsx
+++ src/components/AddAppointment.jsx
@@ -1,7 +1,84 @@
 import React from 'react';
+import FormControl from '@material-ui/core/FormControl';
+import {
+  FormControlLabel,
+  FormHelperText,
+  FormLabel,
+  Input,
+  InputLabel,
+  Paper,
+  Radio,
+  RadioGroup,
+} from '@material-ui/core';
 
 const AddAppointment = () => {
-  return <div>TEST</div>;
+  return (
+    <Paper>
+      <FormControl>
+        <InputLabel htmlFor="my-input">מספר אישי:</InputLabel>
+        <Input id="my-input" aria-describedby="my-helper-text" />
+        <FormLabel component="legend">שאלה ראשונה</FormLabel>
+        <RadioGroup
+          row
+          aria-label="position"
+          name="position"
+          defaultValue="top">
+          <FormControlLabel
+            value="yes1"
+            control={<Radio color="primary" />}
+            label="כן"
+            labelPlacement="start"
+          />
+          <FormControlLabel
+            value="no1"
+            control={<Radio color="primary" />}
+            label="לא"
+            labelPlacement="start"
+          />
+        </RadioGroup>
+
+        <FormLabel component="legend">שאלה שנייה</FormLabel>
+        <RadioGroup
+          row
+          aria-label="position"
+          name="position"
+          defaultValue="top">
+          <FormControlLabel
+            value="yes2"
+            control={<Radio color="primary" />}
+            label="כן"
+            labelPlacement="start"
+          />
+          <FormControlLabel
+            value="no2"
+            control={<Radio color="primary" />}
+            label="לא"
+            labelPlacement="start"
+          />
+        </RadioGroup>
+
+        <FormLabel component="legend">שאלה שלישית</FormLabel>
+        <RadioGroup
+          row
+          aria-label="position"
+          name="position"
+          defaultValue="top">
+          <FormControlLabel
+            value="yes3"
+            control={<Radio color="primary" />}
+            label="כן"
+            labelPlacement="start"
+          />
+          <FormControlLabel
+            value="no3"
+            control={<Radio color="primary" />}
+            label="לא"
+            labelPlacement="start"
+          />
+        </RadioGroup>
+      </FormControl>
+    </Paper>
+  );
 };
 
 export default AddAppointment;
