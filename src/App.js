39ce1f52caa21ae542122d457540255da0356109
AmitFeldman <amit123feldman@gmail.com>
2020-12-24 13:36:25 +0200

    base app bar

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import React from 'react';
 import AddAppointment from './components/AddAppointment';
 import Container from '@material-ui/core/Container';
 import {Route, Switch} from 'react-router-dom';
+import AppBar from '@material-ui/core/AppBar';
+import Palmahim from './Bacha_30_Palmachim.png';
+import Typography from '@material-ui/core/Typography';
+import Avatar from '@material-ui/core/Avatar';
 
 const Component2 = () => <div>2</div>;
 const Component3 = () => <div>3</div>;
@@ -13,10 +17,19 @@ function App() {
     <div className="App">
       <Container>
         <Switch>
-          <Route exact path="/" component={AddAppointment} />
-          <Route path="/2" component={Component2} />
-          <Route path="/3" component={Component3} />
-          <Route path="/4" component={Component4} />
+          <div>
+            <AppBar>
+              <Avatar alt="Palmahim" src={Palmahim} />
+              <Typography variant="h6">
+                ברוכים הבאים למרכז החיסונים בפלמחים
+              </Typography>
+            </AppBar>
+
+            <Route exact path="/" component={AddAppointment} />
+            <Route path="/2" component={Component2} />
+            <Route path="/3" component={Component3} />
+            <Route path="/4" component={Component4} />
+          </div>
         </Switch>
       </Container>
     </div>
