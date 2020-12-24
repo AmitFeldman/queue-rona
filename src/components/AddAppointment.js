355d937dfbdcd378bbff40630bbe6e4c773e5fe3
AlonShirion <shirion.alon@gmail.com>
2020-12-24 13:14:02 +0200

    Good base

--- src/components/AddAppointment.js
+++ src/components/AddAppointment.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import FormControl from '@material-ui/core/FormControl';
 import {
-  AppBar,
   Button,
   FormControlLabel,
   FormLabel,
+  Grid,
   List,
   ListItem,
-  ListItemSecondaryAction,
-  ListItemText,
   Paper,
   Radio,
   RadioGroup,
@@ -17,107 +15,99 @@ import {
 
 const AddAppointment = () => {
   return (
-    <Paper>
-      <List dense>
-        <ListItem>
-          <ListItemText primary="האם פיתחת בעבר תגובה חמורה לאחר שחוסנת בחיסון קורונה או חיסון אחר?" />
-          <ListItemSecondaryAction>
-            <RadioGroup
-              row
-              aria-label="position"
-              name="position"
-              defaultValue="top">
-              <FormControlLabel
-                value="yes1"
-                control={<Radio color="primary" />}
-                label="כן"
-                labelPlacement="start"
-              />
-              <FormControlLabel
-                value="no1"
-                control={<Radio color="primary" />}
-                label="לא"
-                labelPlacement="start"
-              />
-            </RadioGroup>
-          </ListItemSecondaryAction>
-        </ListItem>
-      </List>
-      <FormControl>
-        <TextField placeholder="מספר אישי" />
-        <br />
-        <FormLabel component="legend">
-          האם פיתחת בעבר תגובה חמורה לאחר שחוסנת בחיסון קורונה או חיסון אחר?
-        </FormLabel>
-        <RadioGroup
-          row
-          aria-label="position"
-          name="position"
-          defaultValue="top">
-          <FormControlLabel
-            value="yes1"
-            control={<Radio color="primary" />}
-            label="כן"
-            labelPlacement="start"
-          />
-          <FormControlLabel
-            value="no1"
-            control={<Radio color="primary" />}
-            label="לא"
-            labelPlacement="start"
-          />
-        </RadioGroup>
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justify="center"
+      style={{minHeight: '100vh'}}>
+      <Paper>
+        <FormControl>
+          <List>
+            <ListItem>
+              <TextField placeholder="מספר אישי" />
+            </ListItem>
+            <ListItem>
+              <FormLabel component="legend">
+                האם פיתחת בעבר תגובה חמורה לאחר שחוסנת בחיסון קורונה או חיסון
+                אחר?
+              </FormLabel>
+              <RadioGroup
+                row
+                aria-label="position"
+                name="position"
+                defaultValue="top">
+                <FormControlLabel
+                  value="yes1"
+                  control={<Radio color="primary" />}
+                  label="כן"
+                  labelPlacement="start"
+                />
+                <FormControlLabel
+                  value="no1"
+                  control={<Radio color="primary" />}
+                  label="לא"
+                  labelPlacement="start"
+                />
+              </RadioGroup>
+            </ListItem>
 
-        <br />
-        <FormLabel component="legend">
-          האם ידועה אלרגיה לתרופה/חיסון/מזון?
-        </FormLabel>
-        <RadioGroup
-          row
-          aria-label="position"
-          name="position"
-          defaultValue="top">
-          <FormControlLabel
-            value="yes2"
-            control={<Radio color="primary" />}
-            label="כן"
-            labelPlacement="start"
-          />
-          <FormControlLabel
-            value="no2"
-            control={<Radio color="primary" />}
-            label="לא"
-            labelPlacement="start"
-          />
-        </RadioGroup>
+            <ListItem>
+              <FormLabel component="legend">
+                האם ידועה אלרגיה לתרופה/חיסון/מזון?
+              </FormLabel>
+              <RadioGroup
+                row
+                aria-label="position"
+                name="position"
+                defaultValue="top">
+                <FormControlLabel
+                  value="yes2"
+                  control={<Radio color="primary" />}
+                  label="כן"
+                  labelPlacement="start"
+                />
+                <FormControlLabel
+                  value="no2"
+                  control={<Radio color="primary" />}
+                  label="לא"
+                  labelPlacement="start"
+                />
+              </RadioGroup>
+            </ListItem>
 
-        <br />
-        <FormLabel component="legend">האם את/ה סובל/ת ממחלת חום?</FormLabel>
-        <RadioGroup
-          row
-          aria-label="position"
-          name="position"
-          defaultValue="top">
-          <FormControlLabel
-            value="yes3"
-            control={<Radio color="primary" />}
-            label="כן"
-            labelPlacement="start"
-          />
-          <FormControlLabel
-            value="no3"
-            control={<Radio color="primary" />}
-            label="לא"
-            labelPlacement="start"
-          />
-        </RadioGroup>
-      </FormControl>
+            <ListItem>
+              <FormLabel component="legend">
+                האם את/ה סובל/ת ממחלת חום?
+              </FormLabel>
+              <RadioGroup
+                row
+                aria-label="position"
+                name="position"
+                defaultValue="top">
+                <FormControlLabel
+                  value="yes3"
+                  control={<Radio color="primary" />}
+                  label="כן"
+                  labelPlacement="start"
+                />
+                <FormControlLabel
+                  value="no3"
+                  control={<Radio color="primary" />}
+                  label="לא"
+                  labelPlacement="start"
+                />
+              </RadioGroup>
+            </ListItem>
+          </List>
 
-      <br />
-      <Button variant="contained" color="primary">
-        שלח!
-      </Button>
-    </Paper>
+          <Button variant="contained" color="primary">
+            שלח
+          </Button>
+        </FormControl>
+      </Paper>
+    </Grid>
   );
 };
 
