0cfafa6d70bb5ea44f14007d70950f284dbfc0bb
brilovichgal <brilovichgal@gmail.com>
2020-12-25 11:09:20 +0200

    added vaccineConfirmation component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React from 'react';
 import AddAppointment from './components/AddAppointment';
 import IsSoldierArrived from './components/IsSoldierArrived';
+import VaccineConfirmation from './components/VaccineConfirmation';
 import Container from '@material-ui/core/Container';
 import {Route, Switch} from 'react-router-dom';
 import ScheduleView from './components/ScheduleView';
@@ -18,6 +19,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={AddAppointment} />
           <Route path="/soldierArrival" component={IsSoldierArrived} />
+          <Route path="/vaccineConfirmation" component={VaccineConfirmation} />
           <Route path="/schedule" component={ScheduleView} />
         </Switch>
       </Container>
---
+++ src/components/VaccineConfirmation.js
@@ -0,0 +1,134 @@
+import React, {useEffect, useState} from 'react';
+import FormControl from '@material-ui/core/FormControl';
+import axios from 'axios';
+import {
+  Button,
+  FormControlLabel,
+  FormLabel,
+  Grid,
+  List,
+  ListItem,
+  Paper,
+  Radio,
+  RadioGroup,
+  TextField,
+} from '@material-ui/core';
+
+const RadioOptions = ({value, setValue}) => {
+  return (
+    <RadioGroup row aria-label="position" name="position" defaultValue="top">
+      <FormControlLabel
+        control={
+          <Radio
+            color="primary"
+            checked={value === true}
+            onChange={() => setValue(true)}
+          />
+        }
+        label="כן"
+        labelPlacement="start"
+      />
+      <FormControlLabel
+        control={
+          <Radio
+            color="primary"
+            checked={value === false}
+            onChange={() => setValue(false)}
+          />
+        }
+        label="לא"
+        labelPlacement="start"
+      />
+    </RadioGroup>
+  );
+};
+
+function VaccineConfirmation() {
+  const [soldierId, setId] = React.useState('');
+  const [wasVaccinated, setWasVaccinated] = React.useState(''); /*useState();*/
+
+  async function getResult() {
+    const params = new URLSearchParams();
+    let soldierIdInteger = parseInt(soldierId);
+    let soldierIdWithoutZeroPrefix = soldierIdInteger.toString();
+    let soldierJson = {
+      soldierId: soldierIdWithoutZeroPrefix,
+      wasVaccinated: wasVaccinated,
+    };
+    params.append('0', JSON.stringify(soldierJson));
+    debugger;
+    return await axios.post(
+      'http://127.0.0.1:8080/soldiers/:soldierId/wasVaccinated',
+      params
+    );
+  }
+  function give() {
+    getResult()
+      .then((res) => {
+        alert(res.data.data);
+      })
+      .catch((rej) => {
+        alert(JSON.stringify(rej));
+      });
+  }
+  return (
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justify="center"
+      style={{minHeight: '100vh'}}>
+      <Paper>
+        <FormControl>
+          <List>
+            <ListItem>
+              <TextField
+                placeholder="מספר אישי"
+                value={soldierId}
+                onChange={(e) => setId(e?.target?.value)}
+              />
+            </ListItem>
+            <ListItem>
+              <FormLabel component="legend">האם התחסנת?</FormLabel>
+              <RadioGroup
+                row
+                aria-label="position"
+                name="position"
+                defaultValue="top">
+                <FormControlLabel
+                  control={
+                    <Radio
+                      color="primary"
+                      checked={wasVaccinated === true}
+                      onChange={() => setWasVaccinated(true)}
+                    />
+                  }
+                  label="כן"
+                  labelPlacement="start"
+                />
+                <FormControlLabel
+                  control={
+                    <Radio
+                      color="primary"
+                      checked={wasVaccinated === false}
+                      onChange={() => setWasVaccinated(false)}
+                    />
+                  }
+                  label="לא"
+                  labelPlacement="start"
+                />
+              </RadioGroup>
+            </ListItem>
+          </List>
+
+          <Button variant="contained" color="primary" onClick={give}>
+            שלח
+          </Button>
+        </FormControl>
+      </Paper>
+    </Grid>
+  );
+}
+
+export default VaccineConfirmation;
