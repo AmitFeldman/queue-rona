8556183fde393324758d7047f69c071454471a57
Niv Nachum <romahmahan@gmail.com>
2020-12-26 21:56:21 +0200

    almost finished

--- src/components/AddAppointment.js
+++ src/components/AddAppointment.js
@@ -34,40 +34,12 @@ function SimpleDialog({open}) {
   );
 }
 
-const RadioOptions = ({value, setValue}) => {
-  return (
-    <RadioGroup row aria-label="position" name="position" defaultValue="top">
-      <FormControlLabel
-        control={
-          <Radio
-            color="primary"
-            checked={value === true}
-            onChange={() => setValue(true)}
-          />
-        }
-        label="כן"
-        labelPlacement="start"
-      />
-      <FormControlLabel
-        control={
-          <Radio
-            color="primary"
-            checked={value === false}
-            onChange={() => setValue(false)}
-          />
-        }
-        label="לא"
-        labelPlacement="start"
-      />
-    </RadioGroup>
-  );
-};
-
 const CoolButton = ({text, action}) => {
   const {button} = useStyles();
   return (
     <Button
       className={button}
+      style={{'background-color': 'white'}}
       variant="outlined"
       color="default"
       onClick={() => {
@@ -94,8 +66,8 @@ const useStyles = makeStyles(() =>
     bold: {
       'font-weight': 'bold',
     },
-    white: {
-      input: {
+    text: {
+      '.MuiInputBase-input': {
         'background-color': 'white !important',
       },
     },
@@ -110,17 +82,47 @@ const useStyles = makeStyles(() =>
       width: '50%',
       display: 'inline-block !important',
     },
+    radio: {
+      display: 'none',
+    },
+    radioBox: {
+      backgroundColor: 'white',
+      cursor: 'default',
+      border: 'solid 1px lightGray',
+      display: 'block',
+      'padding-right': '1.5rem',
+      'padding-top': '0.1rem',
+      'padding-bottom': '0.1rem',
+      'padding-left': '1.5rem',
+      'border-radius': '5px',
+      'margin-left': '0',
+      textAlign: 'center',
+      border: 'solid 1px lightGray',
+      outline: '0',
+      cursor: 'pointer',
+    },
   })
 );
 
 const AddAppointment = () => {
-  const {background, center, bold, white, fullWidth, left, grid} = useStyles();
+  const {
+    background,
+    center,
+    bold,
+    fullWidth,
+    left,
+    grid,
+    radioBox,
+    radio,
+    text,
+  } = useStyles();
   const [open, setOpen] = React.useState(false);
 
   const [soldierId, setId] = React.useState('');
   const [q1, setQ1] = React.useState();
   const [q2, setQ2] = React.useState();
   const [q3, setQ3] = React.useState();
+  const [q4, setQ4] = React.useState();
 
   async function getResult() {
     const params = new URLSearchParams();
@@ -148,53 +150,187 @@ const AddAppointment = () => {
         <FormControl>
           <List>
             <ListItem>
-              <label className={center + ' ' + fullWidth}>הזן מספר אישי</label>
+              <label
+                className={center + ' ' + fullWidth}
+                style={{fontSize: '120%'}}>
+                הכנס מספר אישי
+              </label>
             </ListItem>
-            <ListItem>
-              <div className={fullWidth}>
-                <TextField
-                  className={center + ' ' + white}
-                  variant="outlined"
-                  value={id}
-                  onChange={(e) => setId(e?.target?.value)}
-                />
-              </div>
-            </ListItem>
+            <div className={fullWidth}>
+              <TextField
+                className={center + ' ' + text}
+                variant="outlined"
+                value={soldierId}
+                onChange={(e) => setId(e?.target?.value)}
+              />
+            </div>
             <div className={grid + ' ' + center}>
               <Grid container spacing={2}>
                 <Grid item xs={12}>
                   <label className={bold}>אנא ענו על השאלות הבאות</label>
                 </Grid>
-                <Grid item xs={8}>
+                <Grid item xs={12}>
                   <div>
                     <label component="legend">
                       האם סבלת ממחלה עם חום מעל 38° ביומיים האחרונים ?
                     </label>
                   </div>
                 </Grid>
-                <Grid item xs={4}>
-                  <div className={left}>
-                    <RadioOptions value={q1} setValue={setQ1} />
+                <Grid item xs={12}>
+                  <div>
+                    <List style={{padding: 0}}>
+                      <ListItem style={{'padding-right': 0, padding: 0}}>
+                        <FormControlLabel
+                          className={radioBox}
+                          style={{
+                            backgroundColor: q1 === true ? '#000066' : 'white',
+                            color: q1 == true ? 'white' : 'black',
+                          }}
+                          tabindex="1"
+                          control={
+                            <Radio
+                              className={radio}
+                              color="primary"
+                              checked={q1 === true}
+                              onChange={() => {
+                                setQ1(true);
+                              }}
+                            />
+                          }
+                          label="כן"
+                          labelPlacement="start"
+                        />
+                        <FormControlLabel
+                          className={radioBox}
+                          style={{
+                            backgroundColor: q1 === false ? '#000066' : 'white',
+                            color: q1 == false ? 'white' : 'black',
+                            'margin-right': '1rem',
+                          }}
+                          tabindex="1"
+                          control={
+                            <Radio
+                              className={radio}
+                              color="primary"
+                              checked={q1 === false}
+                              onChange={() => {
+                                setQ1(false);
+                              }}
+                            />
+                          }
+                          label="לא"
+                          labelPlacement="start"
+                        />
+                      </ListItem>
+                    </List>
                   </div>
                 </Grid>
-                <Grid item xs={8}>
+                <Grid item xs={12}>
                   <label component="legend">
                     האם ידועה אלרגיה לתרופה, חיסון או מזון ?
                   </label>
                 </Grid>
-                <Grid item xs={4}>
-                  <div className={left}>
-                    <RadioOptions value={q2} setValue={setQ2} />
+                <Grid item xs={12}>
+                  <div>
+                    <List style={{padding: 0}}>
+                      <ListItem style={{'padding-right': 0, padding: 0}}>
+                        <FormControlLabel
+                          className={radioBox}
+                          style={{
+                            backgroundColor: q2 === true ? '#000066' : 'white',
+                            color: q2 == true ? 'white' : 'black',
+                          }}
+                          tabindex="1"
+                          control={
+                            <Radio
+                              className={radio}
+                              color="primary"
+                              checked={q2 === true}
+                              onChange={() => {
+                                setQ2(true);
+                              }}
+                            />
+                          }
+                          label="כן"
+                          labelPlacement="start"
+                        />
+                        <FormControlLabel
+                          className={radioBox}
+                          style={{
+                            backgroundColor: q2 === false ? '#000066' : 'white',
+                            color: q2 == false ? 'white' : 'black',
+                            'margin-right': '1rem',
+                          }}
+                          tabindex="1"
+                          control={
+                            <Radio
+                              className={radio}
+                              color="primary"
+                              checked={q2 === false}
+                              onChange={() => {
+                                setQ2(false);
+                              }}
+                            />
+                          }
+                          label="לא"
+                          labelPlacement="start"
+                        />
+                      </ListItem>
+                    </List>
                   </div>
                 </Grid>
-                <Grid item xs={8}>
+                <Grid item xs={12}>
                   <label component="legend">
                     האם בידך מזרק אפיפן בעקבות תגובה אלרגית משמעותית ?
                   </label>
                 </Grid>
-                <Grid item xs={4}>
-                  <div className={left}>
-                    <RadioOptions value={q3} setValue={setQ3} />
+                <Grid item xs={12}>
+                  <div>
+                    <List style={{padding: 0}}>
+                      <ListItem style={{'padding-right': 0, padding: 0}}>
+                        <FormControlLabel
+                          className={radioBox}
+                          style={{
+                            backgroundColor: q3 === true ? '#000066' : 'white',
+                            color: q3 == true ? 'white' : 'black',
+                          }}
+                          tabindex="1"
+                          control={
+                            <Radio
+                              className={radio}
+                              color="primary"
+                              checked={q3 === true}
+                              onChange={() => {
+                                setQ3(true);
+                              }}
+                            />
+                          }
+                          label="כן"
+                          labelPlacement="start"
+                        />
+                        <FormControlLabel
+                          className={radioBox}
+                          style={{
+                            backgroundColor: q3 === false ? '#000066' : 'white',
+                            color: q3 == false ? 'white' : 'black',
+                            'margin-right': '1rem',
+                          }}
+                          tabindex="1"
+                          control={
+                            <Radio
+                              className={radio}
+                              color="primary"
+                              checked={q3 === false}
+                              onChange={() => {
+                                setQ3(false);
+                              }}
+                            />
+                          }
+                          label="לא"
+                          labelPlacement="start"
+                        />
+                      </ListItem>
+                    </List>
                   </div>
                 </Grid>
                 <Grid item xs={12}>
@@ -204,8 +340,74 @@ const AddAppointment = () => {
                   </label>
                 </Grid>
                 <Grid item xs={12}>
-                  <div className={left}>
-                    <RadioOptions value={q3} setValue={setQ3} />
+                  <div>
+                    <List style={{padding: 0}}>
+                      <ListItem style={{'padding-right': 0, padding: 0}}>
+                        <FormControlLabel
+                          className={radioBox}
+                          style={{
+                            backgroundColor: q4 === 'yes' ? '#000066' : 'white',
+                            color: q4 === 'yes' ? 'white' : 'black',
+                          }}
+                          tabindex="1"
+                          control={
+                            <Radio
+                              className={radio}
+                              color="primary"
+                              checked={q4 === 'yes'}
+                              onChange={() => {
+                                setQ4('yes');
+                              }}
+                            />
+                          }
+                          label="כן"
+                          labelPlacement="start"
+                        />
+                        <FormControlLabel
+                          className={radioBox}
+                          style={{
+                            backgroundColor: q4 === 'no' ? '#000066' : 'white',
+                            color: q4 === 'no' ? 'white' : 'black',
+                            'margin-right': '1rem',
+                          }}
+                          tabindex="1"
+                          control={
+                            <Radio
+                              className={radio}
+                              color="primary"
+                              checked={q4 === 'no'}
+                              onChange={() => {
+                                setQ4('no');
+                              }}
+                            />
+                          }
+                          label="לא"
+                          labelPlacement="start"
+                        />
+                        <FormControlLabel
+                          className={radioBox}
+                          style={{
+                            backgroundColor:
+                              q4 === 'first' ? '#000066' : 'white',
+                            color: q4 === 'first' ? 'white' : 'black',
+                            'margin-right': '1rem',
+                          }}
+                          tabindex="1"
+                          control={
+                            <Radio
+                              className={radio}
+                              color="primary"
+                              checked={q4 === 'first'}
+                              onChange={() => {
+                                setQ4('first');
+                              }}
+                            />
+                          }
+                          label="חיסון קורונה ראשון"
+                          labelPlacement="start"
+                        />
+                      </ListItem>
+                    </List>
                   </div>
                 </Grid>
                 <Grid item xs={12}>
