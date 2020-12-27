01422eca1fb608a53a93b927c9908281c97a6513
brilovichgal <brilovichgal@gmail.com>
2020-12-27 03:08:41 +0200

    did ArrivalToCprStationConfirmation page

--- src/App.js
+++ src/App.js
@@ -16,16 +16,13 @@ function App() {
     <div className="App">
       <NavBar />
 
-
       <Container>
         <Switch>
           <Route exact path="/" component={AddAppointment} />
           <Route path="/soldierArrival" component={IsSoldierArrived} />
           <Route path="/vaccineConfirmation" component={VaccineConfirmation} />
-          <Route path="/schedule" component={ScheduleView} />
-           <Route path="/vaccine" component={VaccineWaitingRoom} />
+          <Route path="/vaccine" component={VaccineWaitingRoom} />
           <Route path="/cpr" component={CPRWaitingRoom} />
-
         </Switch>
       </Container>
     </div>
