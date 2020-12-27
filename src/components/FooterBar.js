30f77193f9155c28e1285ed2361f5ab105e32079
shoval-cohen <shoval835@gmail.com>
2020-12-27 03:47:14 +0200

    Footer 2.0

---
+++ src/components/FooterBar.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import {createStyles, makeStyles} from '@material-ui/core';
+import Avatar from '@material-ui/core/Avatar';
+import Ofek from '../images/OfekLogo.png';
+import Matnam from '../images/MatnamLogo.png';
+import {Image} from 'react';
+
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    bar: {
+      position: 'absolute',
+      width: '100%',
+      height: '10vh',
+      bottom: '20px',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    image: {
+      width: theme.spacing(10),
+      height: theme.spacing(10),
+    },
+  })
+);
+
+const FooterBar = () => {
+  const {image} = useStyles();
+
+  return (
+    <div style={bar}>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          width: '300px',
+        }}>
+        <img width="80" alt="logo" src={Ofek} />
+        <h3>מערכת הספקי חיסון</h3>
+        <img alt="Matnam" width="60" src={Matnam} />
+      </div>
+    </div>
+  );
+};
+
+export default FooterBar;
